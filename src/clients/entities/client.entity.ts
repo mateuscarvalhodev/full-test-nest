5427a42b55fc0ad10a44aa35c033cbe88758ab40
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2025-02-26 22:06:22 -0300

    feat: add user relationship to Client entity

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,16 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Entity('clients')
 export class Client {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 30 })
+  @Column({ type: 'varchar', length: 30, nullable: false })
   name: string;
 
-  @Column({ type: 'float' })
+  @Column({ type: 'float', nullable: false })
   salary: number;
 
-  @Column({ type: 'float' })
+  @Column({ type: 'float', nullable: false })
   enterprisePrice: number;
+
+  @ManyToOne(() => User, (user) => user.clients, { nullable: false })
+  @JoinColumn({ name: 'createdBy' })
+  createdBy: User;
+
 }
