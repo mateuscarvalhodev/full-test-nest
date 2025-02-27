52d9dbcca5edf2c8d7302822860403461514e16d
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2025-02-26 22:04:00 -0300

    feat: add User entity to ClientsModule dependencies

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -3,8 +3,9 @@ import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Client } from './entities/client.entity';
+import { User } from 'src/users/entities/user.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Client])],
+  imports: [TypeOrmModule.forFeature([Client, User])],
   controllers: [ClientsController],
   providers: [ClientsService],
 })
