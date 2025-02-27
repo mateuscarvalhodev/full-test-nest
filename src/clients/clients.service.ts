00cfe203defc405f41ecc7b35e922fc524e5790e
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2025-02-26 22:04:56 -0300

    feat: add user validation and relationship in ClientsService

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -5,20 +5,39 @@ import { Client } from './entities/client.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NotFoundException } from '@nestjs/common';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class ClientsService {
-  constructor(@InjectRepository(Client) private readonly clientRepository: Repository<Client>,) { }
-  create(createClientDto: CreateClientDto): Promise<Client> {
-    const client: Client = new Client()
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientRepository: Repository<Client>,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+  async create(createClientDto: CreateClientDto): Promise<Client> {
+    const user = await this.userRepository.findOneBy({ id: createClientDto.userId })
+    if (!user) {
+      throw new NotFoundException('Não foi encontrado o usuario.')
+    }
+    const client = new Client();
     client.name = createClientDto.name;
-    client.enterprisePrice = createClientDto.enterprisePrice;
     client.salary = createClientDto.salary;
+    client.enterprisePrice = createClientDto.enterprisePrice;
+    client.createdBy = user;
     return this.clientRepository.save(client);
   }
 
-  findAll(): Promise<Client[]> {
-    return this.clientRepository.find();
+  async findAll(userId: number): Promise<Client[]> {
+    const user = await this.userRepository.findOneBy({ id: userId })
+    if (!user) {
+      throw new NotFoundException('Não foi encontrado o usuario.')
+    }
+    return this.clientRepository.find({
+      where: { createdBy: { id: userId } },
+      relations: ['createdBy']
+    });
   }
 
   async findOne(id: number): Promise<Client | null> {
@@ -33,7 +52,11 @@ export class ClientsService {
     return `This action updates a #${id} client`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  async remove(id: number): Promise<{ affected?: number | null }> {
+    const client = await this.clientRepository.findOneBy({ id });
+    if (!client) {
+      throw new NotFoundException(`O Cliente com o ID: ${id} não foi encontrado na nossa base de dados. `)
+    }
+    return this.clientRepository.delete(id)
   }
 }
